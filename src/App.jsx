d87e35bf5834d2bf40f9eb4fa83f9e5f5b1e856e
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-17 19:08:21 -0300

    Fondo segun tamaño de pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-const App = () => {
-  return (
-    <div className="">App</div>
-  )
-}
+// Components
+import { Navbar } from './components';
 
-export default App
+// Assets
+import homeMobile from './assets/home/background-home-mobile.jpg';
+import homeTablet from './assets/home/background-home-tablet.jpg';
+import homeDesktop from './assets/home/background-home-desktop.jpg';
+
+// Hooks
+import useMediaQuery from './hooks/useMediaQuery';
+
+const App = () => {
+  const isDesktop = useMediaQuery('(min-width: 1440px)');
+  const isTablet = useMediaQuery('(min-width: 768px)');
+
+  return (
+    <div className='h-screen relative'>
+      <img
+        src={isDesktop ? homeDesktop : isTablet ? homeTablet : homeMobile}
+        alt='background image'
+        className='w-full h-full absolute top-0'
+      />
+      <header>
+        <Navbar />
+      </header>
+      <main></main>
+    </div>
+  );
+};
+
+export default App;
