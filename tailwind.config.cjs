9619930f411a87bd124b058de4db7f146d42e104
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-18 14:17:48 -0300

    Config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,8 +15,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "D0D6F9",
-        secondary: "0B0D17",
+        primary: "#D0D6F9",
+        secondary: "#0B0D17",
       },
       backgroundImage: {
         'mobile': "url('./src/assets/home/background-home-mobile.jpg')",
