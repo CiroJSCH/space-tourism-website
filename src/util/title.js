8db9aef957243282d5da0b060494f6da20e8ef97
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-21 19:59:57 -0300

    Title based on route

---
+++ src/util/title.js
@@ -0,0 +1,14 @@
+export const title = {
+  '/destination': {
+    number: '01',
+    phrase: 'PICK YOUR DESTINATION',
+  },
+  '/crew': {
+    number: '02',
+    phrase: 'MEET YOUR CREW',
+  },
+  '/technology': {
+    number: '03',
+    phrase: 'SPACE LAUNCH 101',
+  },
+};
