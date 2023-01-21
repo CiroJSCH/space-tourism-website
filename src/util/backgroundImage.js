244dcb147beda65e96410031d9fd5c25210b8c86
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-21 17:41:56 -0300

    Imagenes de fondo

---
+++ src/util/backgroundImage.js
@@ -0,0 +1,42 @@
+// Home
+import homeMobile from '../assets/home/background-home-mobile.jpg';
+import homeTablet from '../assets/home/background-home-tablet.jpg';
+import homeDesktop from '../assets/home/background-home-desktop.jpg';
+
+// Destination
+import destinationMobile from '../assets/home/background-destination-mobile.jpg';
+import destinationTablet from '../assets/home/background-destination-tablet.jpg';
+import destinationDesktop from '../assets/home/background-destination-desktop.jpg';
+
+// Crew
+import crewMobile from '../assets/home/background-crew-mobile.jpg';
+import crewTablet from '../assets/home/background-crew-tablet.jpg';
+import crewDesktop from '../assets/home/background-crew-desktop.jpg';
+
+// Technology
+import techMobile from '../assets/home/background-technology-mobile.jpg';
+import techTablet from '../assets/home/background-technology-tablet.jpg';
+import techDesktop from '../assets/home/background-technology-desktop.jpg';
+
+export const bgImage = {
+  home: {
+    mobile: homeMobile,
+    tablet: homeTablet,
+    desktop: homeDesktop,
+  },
+  destination: {
+    mobile: destinationMobile,
+    tablet: destinationTablet,
+    desktop: destinationDesktop,
+  },
+  crew: {
+    mobile: crewMobile,
+    tablet: crewTablet,
+    desktop: crewDesktop,
+  },
+  technology: {
+    mobile: techMobile,
+    tablet: techTablet,
+    desktop: techDesktop,
+  },
+};
