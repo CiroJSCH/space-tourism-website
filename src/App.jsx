f504dcc10970f869537203caefd63df63354723f
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-21 17:45:58 -0300

    Router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// Libraries
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 // Components
 import { Navbar, Home } from './components';
 
@@ -9,18 +12,22 @@ const App = () => {
   const isTablet = useMediaQuery('(min-width: 768px)');
 
   return (
-    <div
-      className={`h-screen ${
-        isDesktop ? 'bg-desktop' : isTablet ? 'bg-tablet' : 'bg-mobile'
-      } bg-no-repeat bg-cover`}
-    >
-      <header>
-        <Navbar />
-      </header>
-      <main className='xl:h-[calc(100vh-160px-1.75rem)]'>
-        <Home />
-      </main>
-    </div>
+    <BrowserRouter basename='space-tourism-website'>
+      <div
+        className={`h-screen ${
+          isDesktop ? 'bg-desktop' : isTablet ? 'bg-tablet' : 'bg-mobile'
+        } bg-no-repeat bg-cover`}
+      >
+        <header>
+          <Navbar />
+        </header>
+        <main className='xl:h-[calc(100vh-160px-1.75rem)]'>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
