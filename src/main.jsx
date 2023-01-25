5b0792b6c3b1f35b16c6f2d0ca712276251727e7
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-25 19:43:21 -0300

    Basename config

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/space-tourism-website'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
